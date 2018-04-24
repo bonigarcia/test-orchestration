26b2a2afb5f0b46feed89a25806defe3598dbde0
Boni Garcia <bgarcia@gsyc.es>
2018-04-24 12:54:39 +0200

    Define setter for context

--- vars/orchestrator.groovy
+++ vars/orchestrator.groovy
@@ -1,7 +1,13 @@
 class orchestrator implements Serializable {
 
+    def context
+
     def runJob(String jobId) {
-        def job = this.@ctx.build job: jobId, propagate: false
+        def job = this.@context.build job: jobId, propagate: false
         return job.getResult()
+    }
+
+    def setContext(ctx){
+        this.@context = ctx
     }
 }
