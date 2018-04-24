e8a108ffd8b0e0c9be6c6d78c44c1c229eb45313
Boni Garcia <bgarcia@gsyc.es>
2018-04-24 14:58:35 +0200

    Return parallel jobs execution result

--- vars/orchestrator.groovy
+++ vars/orchestrator.groovy
@@ -1,30 +1,37 @@
 class orchestrator {
 
     def context
+    def resultParallel
 
     def runJob(String jobId) {
-        this.@context.stage(jobId) { buildJob(jobId) }
+        this.@context.stage(jobId) { return buildJob(jobId) }
     }
 
     def runJobDependingOn(String result, String job1Id, String job2Id) {
         if (result == 'SUCCESS') {
-            runJob(job1Id)
+            return runJob(job1Id)
         }
         else {
-            runJob(job2Id)
+            return runJob(job2Id)
         }
     }
 
     def runJobsInParallel(String... jobs) {
+        this.resultParallel = true;
         this.@context.stage(jobs.join(", ")) {
             def stepsForParallel = [:]
             for (job in jobs) {
                 def index = job
                 def stepName = "Running ${index}"
-                stepsForParallel[stepName] = { -> buildJob("${index}") }
+                stepsForParallel[stepName] = { -> buildParalleJob("${index}") }
             }
             this.@context.parallel stepsForParallel
+            return resultParallel
         }
+    }
+
+    def buildParalleJob(String jobId) {
+        resultParallel &= buildJob(jobId)
     }
 
     def buildJob(String jobId) {
