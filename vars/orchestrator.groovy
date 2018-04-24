26caf56e2aed3e3a5ed452c8141f7815caef22a8
Boni Garcia <bgarcia@gsyc.es>
2018-04-24 13:18:55 +0200

    Improve stage name for parallel jobs

--- vars/orchestrator.groovy
+++ vars/orchestrator.groovy
@@ -3,15 +3,15 @@ class orchestrator {
     def context
 
     def runJob(String jobId) {
-        this.@context.stage(jobId)
-            buildJob(jobId)
+        this.@context.stage(jobId) { buildJob(jobId) }
     }
 
     def runJobsInParallel(String... jobs) {
-        this.@context.stage("Parallel")
+        this.@context.stage("Parallel jobs : " + jobs.join(", ")) {
             for (job in jobs) {
                 buildJob(job)
             }
+        }
     }
 
     def buildJob(String jobId) {
